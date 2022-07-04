df67769f24f51fb6e728169edddddfae0434a0bb
Erik Salazar <erikdsalazar@yahoo.com>
2022-07-04 00:31:07 -0400

    Add project files.

---
+++ ConsoleAppExample1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace InventorySystem
+{
+    
+    /*
+     * Part object - represents a part
+     * Attributes: Part Number, Part ID, 
+     *             Description, Cost
+     */
+    class Parts
+    {
+        string partNumber { get; set; } 
+        int partId { get; set; }
+        string partDescription { get; set; }
+        double partCost { get; set; }
+
+    }
+
+    class WelcomeMessage
+    {
+        /*
+         * Start Message for the program
+         */
+        public void StartMessage()
+        {
+            System.Console.WriteLine("----------Welcome to the Simple inventory system (SIS)-----------\n");
+            System.Console.WriteLine("Please choose from the following options:\n" +
+                "1. Inventory       2. Add a new part   3. Delete a part\n" +
+                "4. Modify a part   5. Exit");
+        }
+    }
+
+    class MainPage : WelcomeMessage
+    {
+        static void Main(string[] args)
+        {
+            string x = null; //user's option
+
+            WelcomeMessage message = new WelcomeMessage(); //creates message object
+            message.StartMessage(); //creates the start message
+
+            x = System.Console.ReadLine(); 
+            System.Console.WriteLine(x);
+        }
+    }
+
+}
